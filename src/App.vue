289d15e699e4fe3aef88a29f212cdcc60a5d23a8
qinuo0824 <2017256701@qq.com>
2025-06-19 21:54:10 +0800

    feat:业务失败处理和摘取核心响应数据

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,24 @@ const getUser = () => {
     method: 'get'
   })
 }
+
+const login = () => {
+  axios
+    .request({
+      url: 'login/password',
+      method: 'post',
+      data: {
+        mobile: '13211112222',
+        password: 'abc12345'
+      }
+    })
+    .then((res) => {
+      console.log('成功', res)
+    })
+    .catch((err) => {
+      console.log('失败', err)
+    })
+}
 </script>
 
 <template>
@@ -30,6 +48,7 @@ const getUser = () => {
     >
     <van-button @click="store.delUser()" type="primary">退出</van-button>
     <van-button @click="getUser()">获取用户信息</van-button>
+    <van-button @click="login">接口登录</van-button>
   </div>
   <!-- <div class="main">main</div>
   <div class="footer">footer</div> -->
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
 import { useUserStore } from '@/stores'
 import axios from 'axios'
+import { showToast } from 'vant'
 
 const instance = axios.create({
   //1，基础地址，超时时间
@@ -22,9 +23,16 @@ instance.interceptors.request.use(
 
 instance.interceptors.response.use(
   (res) => {
-    //TOD0 3.处理业务失败
-    // TODO 4.摘取核心响应数据
-    return res
+    //3.处理业务失败
+    if (res.data.code !== 10000) {
+      //错误提示
+      showToast(res.data.message || '业务失败')
+      //返回错误的提示
+      return Promise.reject(res.data)
+      //传入 code 将来catch的时候可以使用
+    }
+    //  4.摘取核心响应数据
+    return res.data
   },
   (err) => {
     //TOD0 5. 处理401错误
