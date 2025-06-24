37cd0350e8c7273dfaae2c32c5c8cdd141026608
qinuo0824 <2017256701@qq.com>
2025-06-24 16:22:21 +0800

    feat:组件自动注册

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// import { Button as vanButton } from 'vant';
+</script>
 
 <template>
-  <div class="login-page">login</div>
+  <div class="login-page">
+    login
+
+    <vanButton>按钮</vanButton>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    Components({
+      // dts: false,
+      resolvers: [
+        VantResolver({
+          // importStyle: false
+        })
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
